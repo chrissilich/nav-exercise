2f10ba4824bfc43fb529d468de908cb2b3d7eb4d
obecolette <rebecca.reese@gmail.com>
2015-11-18 12:30:21 -0500

    desktop nav fixed

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -4,10 +4,8 @@
     var data;
 
     if (window.XMLHttpRequest) {
-        // code for IE7+, Firefox, Chrome, Opera, Safari
         navJSON = new XMLHttpRequest();
     } else {
-        // code for IE6, IE5
         navJSON = new ActiveXObject("Microsoft.XMLHTTP");
     }
 
@@ -31,62 +29,43 @@
       var navItemList = data.items;
 
       for (var i = 0; i < navItemList.length; i++) {
-        // make an li  // make <a>
+
         var li = document.createElement('li');
         var a  = document.createElement('a');
         myNav.appendChild(li);
         li.appendChild(a);
-         // populate text and url
+
         a.innerHTML = navItemList[i].label;
         a.setAttribute('href', navItemList[i].url);
+        a.setAttribute('target', '_blank');
 
         var subNavItemList = navItemList[i].items;
         if (subNavItemList.length > 0) {
-          // create ul
+
           var subUl = document.createElement('ul');
           var dropArrow = document.createElement('div');
           subUl.setAttribute('class', navItemList[i].label)
-          // append to originial li
+
           li.appendChild(subUl);
           li.appendChild(dropArrow);
           dropArrow.setAttribute('class', 'drop-arrow');
 
           for (var z = 0; z < subNavItemList.length; z++) {
-            // make an li  // make <a>
-            var subLi = document.createElement('li');
 
+            var subLi = document.createElement('li');
             var subA  = document.createElement('a');
 
             subUl.appendChild(subLi);
             subLi.appendChild(subA);
-             // populate text and url
+
             subA.innerHTML = subNavItemList[z].label;
             subA.setAttribute('href', subNavItemList[z].url);
+            subA.setAttribute('target', '_blank');
           }
         }
-
-
-
-
-        // if statement for items is > [0] loop through array
-          // for each through the array to make sub li and a
       };
     };
-
     navJSON.open("GET", "/api/nav.json", true);
     navJSON.send();
     console.log("complete");
-
-
 })();
-
-// when that succeeds, it should return a long string of JSON
-
-// parse that JSON into real a JS object with an array full of nav item objects in it
-
-
-// var data = JSON.parse(httpRequest.responseText);
-
-// loop through that array, making the nav
-
-
